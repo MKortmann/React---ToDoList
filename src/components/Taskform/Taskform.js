fcf66316cc95f80b6f82c58b0cf8cfa9c37de9a3
sebagruia <sebastiangruia@gmail.com>
2019-11-11 14:58:43 +0100

    Fixed generating list items

--- src/components/Taskform/Taskform.js
+++ src/components/Taskform/Taskform.js
@@ -1,38 +1,55 @@
 import React, { Fragment } from 'react';
 import './Taskform.css';
-import Todoul from '../Todoul/Todoul';
 
 
-const Taskform = ({ inputvalues, onInputChangeNewItem, addNewItem, listButton, inputValuesNewItem }) => {
-    let renderedTaskForm = null;
+const Taskform = ({ inputvalues, onInputChangeNewItem, addNewItem, listButton, listItemsElements }) => {
+    let renderedTaskFormId = null;
     inputvalues.forEach((value, index) => {
         if (listButton === value) {
-            renderedTaskForm = (
+            renderedTaskFormId = (
                 <Fragment>
-                    <form id={value} key={`${value}${index}`} className="taskForm">
-                        <div className="component1">
-                            <input onChange={onInputChangeNewItem} type="text" className="form-control shadow input-new-line" placeholder="New item" aria-label="Insert text" aria-describedby="edit an existing entry field" />
-                        </div>
-                        <div onClick={addNewItem} className="component2 insertEntryButton" role="button">
-                            <i className="far fa-plus-square" aria-hidden="true"></i>
-                            <h5 className="addTask">Add new item</h5>
-                        </div>
-                    </form>
                     <h3 className="todo-name">{value}</h3>
-                    <Todoul id={value} inputValuesNewItem={inputValuesNewItem} />
                 </Fragment>
-
-
             )
         }
-
-
     });
+
+ 
 
 
     return (
         <Fragment>
-            {renderedTaskForm}
+            <form className="taskForm">
+                <div className="component1">
+                    <input onChange={onInputChangeNewItem} type="text" className="form-control shadow input-new-line" placeholder="New item" aria-label="Insert text" aria-describedby="edit an existing entry field" />
+                </div>
+                <div onClick={addNewItem} className="component2 insertEntryButton" role="button">
+                    <i className="far fa-plus-square" aria-hidden="true"></i>
+                    <h5 className="addTask">Add new item</h5>
+                </div>
+            </form>
+            <ul className="todo-list">
+                {renderedTaskFormId}
+                {
+                listItemsElements.map((element,index) => 
+                    (<li key={`${element}${index}`} className="li-item">
+                    <div className="list-component text-secondary">
+                      <div className="check-list">
+                        <i className="far fa-circle" role="button" aria-hidden="true"></i>
+                        <i className="far fa-check-circle text-success hidden" role="button" aria-hidden="true"></i>
+                        <p className="p-text">{element}</p>
+                      </div>
+                      <div className="edit-list">
+                        <i className="far fa-times-circle text-danger" role="button" aria-hidden="true"></i>
+                        <i className="far fa-edit text-info" role="button" data-toggle="modal" data-target="#Modal" aria-hidden="true"></i>
+                        <input className="quantity" type="number" aria-label="Insert a number" name="quantity" min="1" max="20" aria-describedby="number of items of the same kind" placeholder="1" />
+                      </div>
+                    </div>
+                  </li>)
+                )
+                }
+            </ul>
+
         </Fragment>
 
 
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,7 +5,6 @@ import Navigation from '../../components/Navigation/Navigation';
 import Button from 'react-bootstrap/Button';
 import ShoppigListTitles from '../../components/ShoppigListTitles/ShoppigListTitles';
 import Taskform from '../../components/Taskform/Taskform';
-// import Todoul from '../../components/Todoul/Todoul';
 import '../App/App.css';
 
 
@@ -16,14 +15,20 @@ class App extends Component {
       input: '',
       show: false,
       inputvalues: [],
-      listContentVisibility: true,
       inputAddNewItem: '',
       inputValuesNewItem: [],
-      listButton: ''
+      listButton: '',
+      listButtonArray: [
+        // {
+        //   id:'',
+        //   items:[]
+        // }
+      ],
+      allListButtons: []
     };
   }
 
- 
+
   onInputchange = (event) => {
     this.setState({ input: event.target.value });
   }
@@ -33,53 +38,83 @@ class App extends Component {
 
   }
 
-  addNewItem = (event) => {
-    console.log(event);
-    if (this.state.inputAddNewItem !== '') {
-      this.setState({ inputValuesNewItem: [...this.state.inputValuesNewItem, this.state.inputAddNewItem] });
-      this.setState({ inputAddNewItem: '' });
-      console.log(this.state.inputValuesNewItem);
-    }
-  }
-
   printInputValue = () => {
     if (this.state.input !== '') {
       this.setState({ inputvalues: [...this.state.inputvalues, this.state.input] });
       this.setState({ input: '' });
-
-
     }
+    console.log(this.state.inputvalues);
+    this.setState({allListButtons : [...this.state.allListButtons, this.state.input]});
+
+    const allListButtons = [...this.state.allListButtons];
+    if (!allListButtons.includes(this.state.input)) {
+     
+
+      let listButtonArray = [...this.state.listButtonArray];
+      listButtonArray.push({ id: this.state.input, listItems: [] });
+      this.setState({ listButtonArray: listButtonArray });
+      console.log(this.state.listButtonArray);
+    }
+
+
   }
+
   showButton = (event) => {
     this.setState({ listButton: event.target.id });
-
   }
+
+
+
+
+
+
+  addNewItem = (event) => {
+    if (this.state.inputAddNewItem !== '') {
+      this.setState({ inputValuesNewItem: [...this.state.inputValuesNewItem, this.state.inputAddNewItem] });
+      this.setState({ inputAddNewItem: '' });
+    }
+
+    console.log(this.state.inputValuesNewItem);
+
+    let listButtonArray = [...this.state.listButtonArray];
+    
+
+    listButtonArray.forEach((element) => {
+      if (element.id === this.state.listButton) {
+        element.listItems.push(this.state.inputAddNewItem);
+      }
+    });
+  }
+
 
   handleClose = () => this.setState({ show: false });
   handleShow = () => this.setState({ show: true });
 
 
-  render() {
 
+
+
+
+
+  render() {
 
     let componentToBeRender = null;
     this.state.inputvalues.forEach((value, index) => {
       if (this.state.listButton === value) {
         componentToBeRender = (
-        <div id={value} className="list-wraper" key={`${value}${index}`}>
-          <Taskform inputvalues={this.state.inputvalues} 
-                    onInputChangeNewItem={this.onInputChangeNewItem}
-                    addNewItem={this.addNewItem}
-                    listButton={this.state.listButton}
-                    inputValuesNewItem = {this.state.inputValuesNewItem}
-          />
-          {/* <h3 className="todo-name">{value}</h3> */}
-          {/* <ul className="todo-list"></ul> */}
-          {/* <Todoul  inputValuesNewItem={this.state.inputValuesNewItem} /> */}
-        </div>
+          <div id={value} className="list-wraper" key={`${value}${index}`}>
+            <Taskform inputvalues={this.state.inputvalues}
+              onInputChangeNewItem={this.onInputChangeNewItem}
+              addNewItem={this.addNewItem}
+              listButton={this.state.listButton}
+              inputValuesNewItem={this.state.inputValuesNewItem}
+              listItemsElements={this.state.listButtonArray[index].listItems}
+            />
+          </div>
+
         )
       }
-    
+
     })
 
 
@@ -88,57 +123,58 @@ class App extends Component {
 
 
 
-  return(
+    return (
       <Fragment>
-  {/*Bootstrap Modal*/ }
-  < Modal show = { this.state.show } onHide = { this.handleClose } className = "modal-sm" >
-    <Modal.Header closeButton className="modal-header text-white">
-      <Modal.Title>Edit Entry</Modal.Title>
-    </Modal.Header>
+        {/*Bootstrap Modal*/}
+        < Modal show={this.state.show} onHide={this.handleClose} className="modal-sm" >
+          <Modal.Header closeButton className="modal-header text-white">
+            <Modal.Title>Edit Entry</Modal.Title>
+          </Modal.Header>
 
-    <Modal.Body>
-      <input type="text" className="form-control inputForModal" placeholder="Your edit in here" aria-label="edit"
-        aria-describedby="edit an existing entry field" />
-    </Modal.Body>
+          <Modal.Body>
+            <input type="text" className="form-control inputForModal" placeholder="Your edit in here" aria-label="edit"
+              aria-describedby="edit an existing entry field" />
+          </Modal.Body>
 
-    <Modal.Footer>
-      <Button id="modalCancelButton" className="btn text-white">Close</Button>
-      <Button id="modalSaveButton" className="btn bg-warning text-white" >Save</Button>
-    </Modal.Footer>
+          <Modal.Footer>
+            <Button id="modalCancelButton" className="btn text-white">Close</Button>
+            <Button id="modalSaveButton" className="btn bg-warning text-white" >Save</Button>
+          </Modal.Footer>
         </Modal >
-  {/*Modal*/ }
+        {/*Modal*/}
 
-  < StartPage />
+        < StartPage />
 
-  <div className="container-fluid ">
-    <div className="row">
-      <Navigation />
-    </div> {/*End Of Row*/}
+        <div className="container-fluid border border-light">
+          <div className="row">
+            <Navigation />
+          </div> {/*End Of Row*/}
 
-    <div className="row">
-      <div className="col-lg-4 listSummarySection">
-        <form className="form-inline formWraper">
-          <input onChange={this.onInputchange} value={this.state.input} type="text" id="inputNewListItem" className="form-control" placeholder="Name Your List"
-            aria-label="Insert text" aria-describedby="edit an existing entry field" />
-          <button onClick={this.printInputValue} type="button" id="plusButton" className="btn-warning">+</button>
-        </form>
-        <div className="myListSummary-wraper">
-          <button type="button" id="dropdownMenuButton" className="btn btn-outline-warning dropdown-toggle btn-lg btn-block capitalize hidden " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All Your Lists</button>
-          <ShoppigListTitles inputvalues={this.state.inputvalues} showButton={this.showButton} />
-        </div>
-      </div>{/*End Of Col-lg-4*/}
+          <div className="row">
+            <div className="col-lg-4 listSummarySection">
+              <form className="form-inline formWraper">
+                <input onChange={this.onInputchange} value={this.state.input} type="text" id="inputNewListItem" className="form-control" placeholder="Name Your List"
+                  aria-label="Insert text" aria-describedby="edit an existing entry field" />
+                <button onClick={this.printInputValue} type="button" id="plusButton" className="btn-warning">+</button>
+              </form>
+              <div className="myListSummary-wraper">
+                <button type="button" id="dropdownMenuButton" className="btn btn-outline-warning dropdown-toggle btn-lg btn-block capitalize hidden " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All Your Lists</button>
+                <ShoppigListTitles inputvalues={this.state.inputvalues} showButton={this.showButton} />
+              </div>
+            </div>{/*End Of Col-lg-4*/}
+            <div className="col-lg-8 list-content">
+              {componentToBeRender}
+            </div>
 
-      <div className="col-lg-8 list-content">
-        {componentToBeRender}
-      </div>
 
-    </div> {/*End Of Row*/}
-  </div>{/*End Of Container Fluid*/ }
+          </div> {/*End Of Row*/}
+        </div>{/*End Of Container Fluid*/}
       </Fragment >
 
 
     );
   }
 }
+
 
 export default App;
